2740cddfafb353b0363b41519ad214c6e0be703b
stella4545 <stella@linkcoretechnologies.com>
2022-06-14 09:18:31 +0300

    all the commits

--- searching.py
+++ searching.py
@@ -1,21 +1,21 @@
 from abstract import CSVSearch
 
 class SearchCSV(CSVSearch):
-    def __init__(self,data, target, finder ):
+    def __init__(self,data, list, id ):
         self.data = data
-        self.target =target
-        self.finder = finder
+        self.target =list
+        self.finder = id
     
     def search(self):
         left = 0
         right = len(self.data)-1
        
         while left <= right:
-            mid = (left +right) //2
-            if self.target == self.data[mid][self.finder]:
+            mid = (left +right)
+            if list == self.data[mid][self.id[6]]:
                 return self.data[mid]
             else:
-                if self.target < self.data[mid][self.finder]:
+                if list < self.data[mid][self.id[6]]:
                    right = mid -1 
-                if self.target > self.data[mid][self.finder]:              
+                if list > self.data[mid][self.id[6]]:              
                     left = mid +1
